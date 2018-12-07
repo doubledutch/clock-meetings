875423220a1bd16a57e4c50830c2fd555a9ea2d5
Adam Liechty <adam@doubledutch.me>
2018-12-06 16:23:34 -0800

    Upgrade to RN 0.57.5

--- mobile/index.js
+++ mobile/index.js
@@ -17,4 +17,6 @@
 import { AppRegistry } from 'react-native'
 import HomeView from './src/home-view'
 
+console.ignoredYellowBox = ['Warning: View.propTypes has been deprecated']
 AppRegistry.registerComponent('clockmeetings', () => HomeView)
+AppRegistry.registerComponent('section', () => HomeView)
