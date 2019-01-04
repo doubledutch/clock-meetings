623ad2a5dde6c3036f499917744f58bdcff80d84
Adam Liechty <adam@doubledutch.me>
2019-01-04 14:23:22 -0800

    allow upload of personal topics via CSV

--- web/admin/src/App.js
+++ web/admin/src/App.js
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,8 @@ import React, { PureComponent } from 'react'
 import client from '@doubledutch/admin-client'
 import { provideFirebaseConnectorToReactComponent } from '@doubledutch/firebase-connector'
 import { TextInput } from '@doubledutch/react-components'
+import CsvParse from '@vtex/react-csv-parse'
+import { CSVLink } from 'react-csv'
 import '@doubledutch/react-components/lib/base.css'
 
 import './App.css'
@@ -47,12 +49,11 @@ class App extends PureComponent {
     return (
       <div className="vertical space-children">
         <div>
-          Host a fast networking opportunity for your attendees. Choose 3-12
-          slots that attendees can fill (e.g. "Will you be my 'three o'clock?'"),
-          and optionally assign a topic to each slot. Attendees scan each other
-          to confirm their speed-dating-style meeting. Begin advancing the "clock"
-          (suggested 5 minutes per slot) and watch your attendees form new,
-          meaningful connections.
+          Host a fast networking opportunity for your attendees. Choose 3-12 slots that attendees
+          can fill (e.g. &quot;Will you be my &apos;three o&apos;clock?&apos;&quot;), and optionally
+          assign a default topic to each slot. Attendees scan each other to confirm their
+          speed-dating-style meeting. Begin advancing the &quot;clock&quot; (suggested 5 minutes per
+          slot) and watch your attendees form new, meaningful connections.
         </div>
         <label>
           Number of slots:
@@ -87,20 +88,65 @@ class App extends PureComponent {
             </button>
           </div>
         )}
+        <h2>Upload personal topics</h2>
+        <CsvParse
+          className="csv-input"
+          keys={['email', 'topic']}
+          onDataUploaded={this.handleImport}
+          onError={this.handleError}
+          render={onChange => <input type="file" onChange={onChange} />}
+        />
+        <CSVLink className="csvButton" data={this.getCSVTemplate()} filename="personal-topics.csv">
+          Download Template
+        </CSVLink>
         <TextInput
           multiline
-          label="Enter ordered slot topics, one per line. (optional)"
+          label="Enter ordered default slot topics, one per line (optional). These are used if attendees do not have personal topics selected."
           onChange={this.updateTopics}
           value={topics}
         />
         <div className="footer">
           <button className="dd-bordered destructive" onClick={this.clear}>
-            Clear all attendees' scheduled meetings!
+            Clear all attendees&apos; scheduled meetings!
           </button>
         </div>
       </div>
     )
   }
+
+  getCSVTemplate = () => [
+    {
+      email: 'jean@valjean.com',
+      topic:
+        'Personal: What are good ways to meet friends in a new city? Professional: How do you approach a salary negotiation?',
+    },
+  ]
+
+  handleImport = data => {
+    const { fbc } = this.props
+    const attendeePromises = data.map(({ email, topic }) =>
+      client
+        .getAttendees(email)
+        .then(as => (as.length ? { ...as[0], topic } : null))
+        .catch(() => null),
+    )
+    Promise.all(attendeePromises)
+      .then(topics => topics.filter(x => x && x.id && x.topic != null))
+      .then(attendeesWithTopics => {
+        const attendeeUpdates = attendeesWithTopics.reduce((obj, attendeeWithTopic) => {
+          obj[attendeeWithTopic.id] = { ...attendeeWithTopic, id: null }
+          return obj
+        }, {})
+        fbc.database.public.usersRef().update(attendeeUpdates)
+        let message = `Topics updated for ${attendeesWithTopics.length} attendees.`
+        if (data.length > attendeesWithTopics.length) {
+          message += ` ${data.length - attendeesWithTopics.length} attendees were not found.`
+        }
+        window.alert(message)
+      })
+  }
+
+  handleError = error => console.error(error)
 
   updatePublicNumber = prop => e => {
     this.props.fbc.database.public.adminRef(prop).set(+e.target.value)
